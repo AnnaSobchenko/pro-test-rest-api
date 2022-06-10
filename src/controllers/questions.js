7f1f3a14f04774f0916aa0caa456f2a901d11dcb
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-06-10 08:35:43 +0300

    Get right answers

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -1,4 +1,9 @@
-const { technicalQuestion, theoryQuestion } = require("../models/questions");
+const {
+	technicalQuestion,
+	theoryQuestion,
+	theoryQuestionAll,
+	technicalQuestionAll,
+} = require("../models/questions");
 
 const getTheoryQuestion = async (req, res, next) => {
   const questions = await theoryQuestion();
@@ -10,7 +15,19 @@ const getTechnicalQuestion = async (req, res, next) => {
   res.status(200).send(questions);
 };
 
+const getTheoryQuestionAll = async (req, res, next) => {
+	const questions = await theoryQuestionAll();
+	res.status(200).send(questions);
+};
+
+const getTechnicalQuestionAll = async (req, res, next) => {
+	const questions = await technicalQuestionAll();
+	res.status(200).send(questions);
+};
+
 module.exports = {
-  getTechnicalQuestion,
-  getTheoryQuestion,
+	getTechnicalQuestion,
+	getTheoryQuestion,
+	getTheoryQuestionAll,
+	getTechnicalQuestionAll,
 };

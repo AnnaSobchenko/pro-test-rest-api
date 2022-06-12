fb82fe5f9ff0903c5c850d2680b7a544c0af1b8a
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2022-06-12 12:02:34 +0300

    update contactModel

--- src/db/contactModel.js
+++ src/db/contactModel.js
@@ -13,6 +13,20 @@ const contactsSchema = new mongoose.Schema({
     type: String,
     // required: [true, "Description is required"],
   },
+  links: {
+    github : {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    resume: {
+      type: String,
+    },
+  },
 });
 
 const Contacts = mongoose.model("contacts", contactsSchema);

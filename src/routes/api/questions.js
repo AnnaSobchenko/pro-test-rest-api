56b85f96363793ce1b8324314a18b30e2857b3c2
AnnaSobchenko <annsbchnk@gmail.com>
2022-06-15 12:17:45 +0300

    dev=>main

--- src/routes/api/questions.js
+++ src/routes/api/questions.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { catchErrors } = require("../../middlewares/catchErrors");
 
-const {
-  getQuestion,
-  checkQuestion,
-} = require("../../controllers/questions");
+const { getQuestion, checkQuestion } = require("../../controllers/questions");
 
 router.get("/:type", catchErrors(getQuestion));
 router.post("/check/:type", catchErrors(checkQuestion));

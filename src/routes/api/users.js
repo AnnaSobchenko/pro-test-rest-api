e88f6b34892d0412566f369fb6b61e711af4934c
Roman Havryliuk <gavru4r@gmail.com>
2022-06-21 13:36:00 +0300

    start gooogle auth

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const authorize = require("../../middlewares/authorize");
 const {
-	signupUserControl,
-	signinUserControl,
-	logoutUserControl,
-	currentUserControl,
-	refreshTokenControl,
+  signupUserControl,
+  signinUserControl,
+  logoutUserControl,
+  currentUserControl,
+  refreshTokenControl,
 } = require("../../controllers/users");
 const { schemas } = require("../../db/questionsModel");
 const {
-	catchLogErrors,
-	catchSignupErrors,
-	catchErrors,
+  catchLogErrors,
+  catchSignupErrors,
+  catchErrors,
 } = require("../../middlewares/catchErrors");
 
 const router = express.Router();
 
 router.post(
-	"/signup",
-	schemas.postAuthValidation,
-	catchSignupErrors(signupUserControl)
+  "/signup",
+  schemas.postAuthValidation,
+  catchSignupErrors(signupUserControl)
 );
-router.post("/login", schemas.postAuthValidation, catchLogErrors(signinUserControl));
+router.post(
+  "/login",
+  schemas.postAuthValidation,
+  catchLogErrors(signinUserControl)
+);
 router.post("/logout", authorize, catchErrors(logoutUserControl));
 router.get("/current", authorize, catchErrors(currentUserControl));
 router.post("/refresh", authorize, catchErrors(refreshTokenControl));
+
+router.get("/google", (req, res) => {
+  console.log("req :>> ", req);
+  res.render("login");
+});
 
 module.exports = router;
